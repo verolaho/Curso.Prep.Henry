7d8917e761f5dff8c4882e8669fca34f63fa81f1
Veronica lahourcade <verolaho>
2022-09-30 00:15:03 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,8 +1,7 @@
-// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+// En estas primeras 6 preguntas, reemplaza `hola` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
-
+const nuevaString = 'null';
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 
@@ -10,7 +9,7 @@ const nuevoNum = null;
 const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - null === 5
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
